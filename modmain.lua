09e82db3e8f9ab0d317a37c5229705d64ee9e23e
秋一 <civi@rcg.fun>
2021-06-30 21:25:51 +0800

    move scripts/modules to main

--- modmain.lua
+++ modmain.lua
@@ -6,17 +6,18 @@ PrefabFiles = {
 
 GlassicAPI.InitCharacterAssets("miotan", "FEMALE", Assets)
 GlassicAPI.InitMinimapAtlas("miotan_minimap", Assets)
-
 GlassicAPI.RegisterItemAtlas("miotan_inventoryimages", Assets)
+
+local main_files = {
+	"eatfuel",
+	"fuelactions",
+	"prefabskin",
+	"sanity_reward",
+	"tradefuel"
+}
+
+for _,v in pairs(main_files) do modimport("main/"..v) end
 
 -- Custom speech strings and skin
 modimport("strings/miotan_init.lua")
-modimport("scripts/modules/prefabskin.lua")
-
 modimport("strings/miotan_str"..(table.contains({"zh","chs","cht"}, GLOBAL.LanguageTranslator.defaultlang) and "_chs" or "")..".lua")
-
--- functions --
-modimport("scripts/modules/eatfuel.lua")
-modimport("scripts/modules/tradefuel.lua")
-modimport("scripts/modules/fuelactions.lua")
-modimport("scripts/modules/sanity_reward.lua")
